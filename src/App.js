963db26ba2e69437c4dd76a44cd35f077568869e
rajannahar <R_N101@msn.com>
2020-01-02 13:44:08 +0000

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 
 function App() {
+
+  const [address, setAddress] = useState("")
+  const [coordinates, setCoordinates] = useState({lat: null, lng: null})
+
+  const handleSelect = async (value) => {
+    const results = await geocodeByAddress(value)
+    const latLng = await getLatLng(results[0])
+    // console.log(results)
+    setAddress(value)
+    setCoordinates(latLng)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PlacesAutocomplete value={address} onChange={setAddress} onSelect={handleSelect}>
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <p>Lat: {coordinates.lat}</p>
+            <p>Lng: {coordinates.lng }</p>
+
+            <input {...getInputProps({ placeholder: "Type address" })} />
+
+            <div>
+              { loading ? <div>Loading...</div> : null }
+            </div>
+
+            { suggestions.map(suggestion => {
+
+              const style = {
+                backgroundColor: suggestion.active ? "#41b6e6" : "#ffffff"
+              }
+
+              return <div {...getSuggestionItemProps(suggestion, { style })}>{suggestion.description}</div>
+            }) }
+
+          </div>
+        )}
+      </PlacesAutocomplete>
     </div>
   );
 }
